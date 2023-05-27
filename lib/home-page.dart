4da6166bb1040d815817f702991c232967b61498
Shiva Kumar <shivakumar@Shivas-Air.domain.name>
2023-05-27 17:19:22 +0530

    completed otp authentication

---
+++ lib/home-page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  @override
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome Home'),
+      ),
+    );
+  }
+}
